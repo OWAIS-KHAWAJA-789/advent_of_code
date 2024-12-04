b678940e3f80128fec706101ce2f114a9ea0e207
owais <owaiskhawaja789@gmail.com>
2024-12-04 19:44:25 +0500

    Add or update code for day 4 of 2024

--- git_operations.sh
+++ git_operations.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
-# Check if day num is provided as argument
 if [ -z "$1" ]; then
     echo "Error: Please provide a day number as an argument."
-    echo "Usage: ./git_operations.sh <day_number>"
+    echo "Usage: ./git_operations.sh <day_number> [custom_commit_message]"
     exit 1
 fi
 
 DAY_NUMBER=$1
 YEAR="2024"
 
-# if the specific day folder exist
 DAY_FOLDER="$YEAR/d$DAY_NUMBER"
 if [ ! -d "$DAY_FOLDER" ]; then
     echo "Error: The folder for day $DAY_NUMBER does not exist. Please create the folder and add your files before running the script."
     exit 1
 fi
 
-# Stage all changes 
+if [ -n "$2" ]; then
+    COMMIT_MESSAGE="$2"
+else
+    COMMIT_MESSAGE="Add or update code for day $DAY_NUMBER of $YEAR"
+fi
 echo "Staging changes..."
 git add .
-
-# Commit changes 
 echo "Committing changes..."
-git commit -m "Add or update code for day $DAY_NUMBER of $YEAR"
-
-# Push changs to github
+git commit -m "$COMMIT_MESSAGE"
 echo "Pushing changes to GitHub..."
 git push origin master
 
-echo "Done! Day $DAY_NUMBER changes have been committed and and pushed."
+echo "Done! Day $DAY_NUMBER changes have been committed and pushed."
+
